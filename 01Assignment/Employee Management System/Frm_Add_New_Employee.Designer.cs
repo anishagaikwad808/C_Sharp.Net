70ba8f4ca03e15c8347661735a520bdadbce3ea3
anishagaikwad808 <103376736+anishagaikwad808@users.noreply.github.com>
2023-07-03 13:19:56 +0530

    Add files via upload

---
+++ 01Assignment/Employee Management System/Frm_Add_New_Employee.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace _01_Employee_Management_System
+{
+    partial class Frm_Add_New_Employee
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Add_Employee_Information = new System.Windows.Forms.Label();
+            this.lbl_Id = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Mob_No = new System.Windows.Forms.Label();
+            this.lbl_DOB = new System.Windows.Forms.Label();
+            this.lbl_Designation = new System.Windows.Forms.Label();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.tb_Mob_No = new System.Windows.Forms.TextBox();
+            this.tb_Id = new System.Windows.Forms.TextBox();
+            this.dtp_DOB = new System.Windows.Forms.DateTimePicker();
+            this.cmb_Designation = new System.Windows.Forms.ComboBox();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Employee_List = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Add_Employee_Information
+            // 
+            this.lbl_Add_Employee_Information.AutoSize = true;
+            this.lbl_Add_Employee_Information.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbl_Add_Employee_Information.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Add_Employee_Information.Location = new System.Drawing.Point(123, 37);
+            this.lbl_Add_Employee_Information.Name = "lbl_Add_Employee_Information";
+            this.lbl_Add_Employee_Information.Size = new System.Drawing.Size(734, 69);
+            this.lbl_Add_Employee_Information.TabIndex = 0;
+            this.lbl_Add_Employee_Information.Text = "Add Employee Information";
+            // 
+            // lbl_Id
+            // 
+            this.lbl_Id.AutoSize = true;
+            this.lbl_Id.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_Id.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Id.ForeColor = System.Drawing.Color.White;
+            this.lbl_Id.Location = new System.Drawing.Point(74, 167);
+            this.lbl_Id.Name = "lbl_Id";
+            this.lbl_Id.Size = new System.Drawing.Size(43, 37);
+            this.lbl_Id.TabIndex = 1;
+            this.lbl_Id.Text = "Id";
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.ForeColor = System.Drawing.Color.White;
+            this.lbl_Name.Location = new System.Drawing.Point(64, 247);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(103, 37);
+            this.lbl_Name.TabIndex = 2;
+            this.lbl_Name.Text = "Name";
+            // 
+            // lbl_Mob_No
+            // 
+            this.lbl_Mob_No.AutoSize = true;
+            this.lbl_Mob_No.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_Mob_No.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Mob_No.ForeColor = System.Drawing.Color.White;
+            this.lbl_Mob_No.Location = new System.Drawing.Point(64, 327);
+            this.lbl_Mob_No.Name = "lbl_Mob_No";
+            this.lbl_Mob_No.Size = new System.Drawing.Size(161, 37);
+            this.lbl_Mob_No.TabIndex = 3;
+            this.lbl_Mob_No.Text = "Mobile No";
+            // 
+            // lbl_DOB
+            // 
+            this.lbl_DOB.AutoSize = true;
+            this.lbl_DOB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_DOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DOB.ForeColor = System.Drawing.Color.White;
+            this.lbl_DOB.Location = new System.Drawing.Point(64, 411);
+            this.lbl_DOB.Name = "lbl_DOB";
+            this.lbl_DOB.Size = new System.Drawing.Size(104, 37);
+            this.lbl_DOB.TabIndex = 4;
+            this.lbl_DOB.Text = "D.O.B";
+            // 
+            // lbl_Designation
+            // 
+            this.lbl_Designation.AutoSize = true;
+            this.lbl_Designation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_Designation.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Designation.ForeColor = System.Drawing.Color.White;
+            this.lbl_Designation.Location = new System.Drawing.Point(64, 488);
+            this.lbl_Designation.Name = "lbl_Designation";
+            this.lbl_Designation.Size = new System.Drawing.Size(186, 37);
+            this.lbl_Designation.TabIndex = 5;
+            this.lbl_Designation.Text = "Designation";
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Name.Location = new System.Drawing.Point(471, 247);
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(421, 44);
+            this.tb_Name.TabIndex = 2;
+            // 
+            // tb_Mob_No
+            // 
+            this.tb_Mob_No.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Mob_No.Location = new System.Drawing.Point(471, 327);
+            this.tb_Mob_No.MaxLength = 10;
+            this.tb_Mob_No.Name = "tb_Mob_No";
+            this.tb_Mob_No.Size = new System.Drawing.Size(421, 44);
+            this.tb_Mob_No.TabIndex = 3;
+            // 
+            // tb_Id
+            // 
+            this.tb_Id.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Id.Location = new System.Drawing.Point(471, 167);
+            this.tb_Id.Name = "tb_Id";
+            this.tb_Id.Size = new System.Drawing.Size(421, 44);
+            this.tb_Id.TabIndex = 1;
+            // 
+            // dtp_DOB
+            // 
+            this.dtp_DOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_DOB.Location = new System.Drawing.Point(471, 419);
+            this.dtp_DOB.Name = "dtp_DOB";
+            this.dtp_DOB.Size = new System.Drawing.Size(421, 44);
+            this.dtp_DOB.TabIndex = 4;
+            this.dtp_DOB.Value = new System.DateTime(2010, 1, 31, 0, 0, 0, 0);
+            // 
+            // cmb_Designation
+            // 
+            this.cmb_Designation.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Designation.FormattingEnabled = true;
+            this.cmb_Designation.Items.AddRange(new object[] {
+            "Manager",
+            "Developer",
+            "Designer",
+            "Testor"});
+            this.cmb_Designation.Location = new System.Drawing.Point(471, 497);
+            this.cmb_Designation.Name = "cmb_Designation";
+            this.cmb_Designation.Size = new System.Drawing.Size(421, 45);
+            this.cmb_Designation.TabIndex = 5;
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_Refresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Refresh.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_Refresh.Location = new System.Drawing.Point(110, 590);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(129, 42);
+            this.btn_Refresh.TabIndex = 7;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = false;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Save.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_Save.Location = new System.Drawing.Point(384, 590);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(129, 42);
+            this.btn_Save.TabIndex = 6;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = false;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Employee_List
+            // 
+            this.btn_Employee_List.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_Employee_List.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Employee_List.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_Employee_List.Location = new System.Drawing.Point(646, 590);
+            this.btn_Employee_List.Name = "btn_Employee_List";
+            this.btn_Employee_List.Size = new System.Drawing.Size(264, 42);
+            this.btn_Employee_List.TabIndex = 8;
+            this.btn_Employee_List.Text = "Employee List";
+            this.btn_Employee_List.UseVisualStyleBackColor = false;
+            this.btn_Employee_List.Click += new System.EventHandler(this.btn_Employee_List_Click);
+            // 
+            // Frm_Add_New_Employee
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(978, 644);
+            this.Controls.Add(this.btn_Employee_List);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.cmb_Designation);
+            this.Controls.Add(this.dtp_DOB);
+            this.Controls.Add(this.tb_Id);
+            this.Controls.Add(this.tb_Mob_No);
+            this.Controls.Add(this.tb_Name);
+            this.Controls.Add(this.lbl_Designation);
+            this.Controls.Add(this.lbl_DOB);
+            this.Controls.Add(this.lbl_Mob_No);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.lbl_Id);
+            this.Controls.Add(this.lbl_Add_Employee_Information);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frm_Add_New_Employee";
+            this.Text = "Frm_Add_New_Employee";
+            this.Load += new System.EventHandler(this.Frm_Add_New_Employee_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Add_Employee_Information;
+        private System.Windows.Forms.Label lbl_Id;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Mob_No;
+        private System.Windows.Forms.Label lbl_DOB;
+        private System.Windows.Forms.Label lbl_Designation;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.TextBox tb_Mob_No;
+        private System.Windows.Forms.TextBox tb_Id;
+        private System.Windows.Forms.DateTimePicker dtp_DOB;
+        private System.Windows.Forms.ComboBox cmb_Designation;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Employee_List;
+    }
+}
---
+++ 01Assignment/Employee Management System/frm_View_Employee_List.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace _01_Employee_Management_System
+{
+    partial class frm_View_Employee_List
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lbl_Employee_Details_List = new System.Windows.Forms.Label();
+            this.dgv_Employee_List = new System.Windows.Forms.DataGridView();
+            this.employeesDetailsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btn_Add_New_Employee = new System.Windows.Forms.Button();
+            this.employee_App_DBDataSet1 = new _01_Employee_Management_System.Employee_App_DBDataSet1();
+            this.employeesDetailsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.employees_DetailsTableAdapter = new _01_Employee_Management_System.Employee_App_DBDataSet1TableAdapters.Employees_DetailsTableAdapter();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Employee_List)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesDetailsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employee_App_DBDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesDetailsBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Employee_Details_List
+            // 
+            this.lbl_Employee_Details_List.AutoSize = true;
+            this.lbl_Employee_Details_List.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lbl_Employee_Details_List.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Employee_Details_List.Location = new System.Drawing.Point(174, 18);
+            this.lbl_Employee_Details_List.Name = "lbl_Employee_Details_List";
+            this.lbl_Employee_Details_List.Size = new System.Drawing.Size(606, 69);
+            this.lbl_Employee_Details_List.TabIndex = 0;
+            this.lbl_Employee_Details_List.Text = "Employee Details List";
+            // 
+            // dgv_Employee_List
+            // 
+            this.dgv_Employee_List.AutoGenerateColumns = false;
+            this.dgv_Employee_List.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Employee_List.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_Employee_List.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_Employee_List.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Employee_List.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5});
+            this.dgv_Employee_List.DataSource = this.employeesDetailsBindingSource1;
+            this.dgv_Employee_List.Location = new System.Drawing.Point(2, 103);
+            this.dgv_Employee_List.Name = "dgv_Employee_List";
+            this.dgv_Employee_List.RowHeadersWidth = 62;
+            this.dgv_Employee_List.RowTemplate.Height = 28;
+            this.dgv_Employee_List.Size = new System.Drawing.Size(975, 486);
+            this.dgv_Employee_List.TabIndex = 1;
+            // 
+            // employeesDetailsBindingSource
+            // 
+            this.employeesDetailsBindingSource.DataMember = "Employees_Details";
+            // 
+            // btn_Add_New_Employee
+            // 
+            this.btn_Add_New_Employee.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_Add_New_Employee.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add_New_Employee.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_Add_New_Employee.Location = new System.Drawing.Point(307, 595);
+            this.btn_Add_New_Employee.Name = "btn_Add_New_Employee";
+            this.btn_Add_New_Employee.Size = new System.Drawing.Size(319, 46);
+            this.btn_Add_New_Employee.TabIndex = 2;
+            this.btn_Add_New_Employee.Text = "Add New Employee";
+            this.btn_Add_New_Employee.UseVisualStyleBackColor = false;
+            // 
+            // employee_App_DBDataSet1
+            // 
+            this.employee_App_DBDataSet1.DataSetName = "Employee_App_DBDataSet1";
+            this.employee_App_DBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeesDetailsBindingSource1
+            // 
+            this.employeesDetailsBindingSource1.DataMember = "Employees_Details";
+            this.employeesDetailsBindingSource1.DataSource = this.employee_App_DBDataSet1;
+            // 
+            // employees_DetailsTableAdapter
+            // 
+            this.employees_DetailsTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Name";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Mobile_No";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Mobile_No";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "D_O_B";
+            this.dataGridViewTextBoxColumn4.HeaderText = "D_O_B";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Designation";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Designation";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 8;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // frm_View_Employee_List
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(978, 644);
+            this.Controls.Add(this.btn_Add_New_Employee);
+            this.Controls.Add(this.dgv_Employee_List);
+            this.Controls.Add(this.lbl_Employee_Details_List);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_View_Employee_List";
+            this.Text = "View Employee List";
+            this.Load += new System.EventHandler(this.Frm_View_Employee_List_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Employee_List)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesDetailsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employee_App_DBDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeesDetailsBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Employee_Details_List;
+        private System.Windows.Forms.DataGridView dgv_Employee_List;
+        private System.Windows.Forms.BindingSource employeesDetailsBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mobileNoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dOBDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn designationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btn_Add_New_Employee;
+        private Employee_App_DBDataSet1 employee_App_DBDataSet1;
+        private System.Windows.Forms.BindingSource employeesDetailsBindingSource1;
+        private Employee_App_DBDataSet1TableAdapters.Employees_DetailsTableAdapter employees_DetailsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+    }
+}
